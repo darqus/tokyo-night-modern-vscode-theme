1a8e21d07f393ac6e67c8dfced9066129c5197d9
darqus <voladores@inbox.ru>
2025-10-10 01:50:45 +0300

    chore: remove legacy documentation and source files

--- src/core/simplified/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Упрощенная архитектура - единая точка экспорта
- */
-export { ThemeGenerator } from './ThemeGenerator'
-export { ConfigManager } from './ConfigManager'
-export { PluginSystem } from './PluginSystem'
-export type { SimpleThemeConfig } from './ThemeGenerator'
-export type { SimplePlugin } from './PluginSystem'
