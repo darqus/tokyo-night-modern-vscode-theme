e9513a0bbc781584ff3f9f24d80a10dec180ffee
darqus <voladores@inbox.ru>
2025-10-14 03:34:46 +0300

    refactor(theme): replace hardcoded colors with palette references in git decoration and scm graph modules

--- src/theme/ui-modules/git-decoration.ts
+++ src/theme/ui-modules/git-decoration.ts
@@ -2,32 +2,33 @@
  * Git decoration colors - colors for Git status indicators in UI
  * These colors are used for Git decorations in the editor and file explorer
  */
+import { palette } from '../palette/index'
 
 export const gitDecorationTokens = {
   // Modified resource - for files that have been modified
-  'gitDecoration.modifiedResourceForeground': '#B79DFF',
+  'gitDecoration.modifiedResourceForeground': palette.purple,
   
   // Ignored resource - for files that are ignored by Git
-  'gitDecoration.ignoredResourceForeground': '#515C7E',
+  'gitDecoration.ignoredResourceForeground': palette.textDisabled,
   
   // Deleted resource - for files that have been deleted
-  'gitDecoration.deletedResourceForeground': '#FF5370',
+  'gitDecoration.deletedResourceForeground': palette.error,
   
   // Renamed resource - for files that have been renamed
-  'gitDecoration.renamedResourceForeground': '#A779F3',
+  'gitDecoration.renamedResourceForeground': palette.secondary,
   
   // Added resource - for files that have been added
-  'gitDecoration.addedResourceForeground': '#A779F3',
+  'gitDecoration.addedResourceForeground': palette.secondary,
   
   // Untracked resource - for files that are not tracked by Git
-  'gitDecoration.untrackedResourceForeground': '#7BCCFC',
+  'gitDecoration.untrackedResourceForeground': palette.info,
   
   // Conflicting resource - for files with merge conflicts
-  'gitDecoration.conflictingResourceForeground': '#F2B04E',
+  'gitDecoration.conflictingResourceForeground': palette.warning,
   
   // Stage deleted resource - for deleted files in the staging area
-  'gitDecoration.stageDeletedResourceForeground': '#FF5370',
+  'gitDecoration.stageDeletedResourceForeground': palette.error,
   
   // Stage modified resource - for modified files in the staging area
-  'gitDecoration.stageModifiedResourceForeground': '#B79DFF',
+  'gitDecoration.stageModifiedResourceForeground': palette.purple,
 }
