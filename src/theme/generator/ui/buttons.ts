8f8a7aa683df909d95200a94979560a9c4a805b0
darqus <voladores@inbox.ru>
2025-10-29 21:12:56 +0300

    style(theme): update button colors in Tokyo Night theme
    - Replace darken function with mix function for better color blending
    - Update prominent button and regular button backgrounds to special.badge color
    - Adjust hover background colors using mix function for a smoother transition
    - Update color values in theme JSON file to reflect new colors

--- src/theme/generator/ui/buttons.ts
+++ src/theme/generator/ui/buttons.ts
@@ -1,17 +1,21 @@
 import { palette } from '../../palette'
-import { darken } from '../../utils/color'
+import { mix } from '../../utils/color'
 
 export function generateButtonColors(): Record<string, string> {
   const { blue, special, ui } = palette
 
   return {
-    'extensionButton.prominentBackground': darken(blue.medium, 0.6),
-    'extensionButton.prominentHoverBackground': darken(blue.medium, 0.5),
+    'extensionButton.prominentBackground': special.badge,
+    'extensionButton.prominentHoverBackground': mix(
+      special.badge,
+      blue.medium,
+      0.3
+    ),
     'extensionButton.prominentForeground': ui.white,
     'extensionBadge.remoteBackground': special.badge,
     'extensionBadge.remoteForeground': ui.white,
-    'button.background': darken(blue.medium, 0.6),
-    'button.hoverBackground': darken(blue.medium, 0.5),
+    'button.background': special.badge,
+    'button.hoverBackground': mix(special.badge, blue.medium, 0.3),
     'button.secondaryBackground': special.inactive,
     'button.foreground': ui.white,
     'progressBar.background': blue.primary,
