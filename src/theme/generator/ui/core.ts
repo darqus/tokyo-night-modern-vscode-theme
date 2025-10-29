27a07dc306fc0f3dc3af231661e21936bf8965b1
darqus <voladores@inbox.ru>
2025-10-29 19:48:14 +0300

    style(vscode-theme-darcula): update color values and adjust styles
    - Update color values for various UI elements
    - Adjust styles for list, editor, and other components
    - Modify font styles for Markdown and other text elements
    - Update colors for Git and debug related elements
    - Make minor adjustments to chat and notification styles

--- src/theme/generator/ui/core.ts
+++ src/theme/generator/ui/core.ts
@@ -5,16 +5,16 @@ export function generateCoreColors(): Record<string, string> {
   const { fg, bg, special, blue, ui } = palette
 
   return {
-    foreground: fg.medium,
-    descriptionForeground: fg.dim,
+    foreground: fg.bright,
+    descriptionForeground: fg.medium,
     disabledForeground: special.disabled,
-    focusBorder: lighten(bg.light, 0.04),
+    focusBorder: alpha(blue.light, 0.3),
     errorForeground: fg.dim,
     'widget.shadow': alpha(ui.white, 0),
     'scrollbar.shadow': alpha(palette.ui.scrollbarShadow, 0.2),
     'badge.background': special.badge,
     'badge.foreground': ui.badgeForeground,
-    'icon.foreground': fg.medium,
+    'icon.foreground': fg.bright,
     'settings.headerForeground': blue.light,
     'window.activeBorder': bg.darkest,
     'window.inactiveBorder': bg.darkest,
