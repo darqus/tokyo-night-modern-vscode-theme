db645d882775b0bb6d553c2dc25024eae694631f
darqus <voladores@inbox.ru>
2025-11-10 17:18:57 +0300

    refactor(theme): remove unnecessary comments from semantic token config
    - Remove color code comments that were redundant with the color name
    - Consistent formatting of token rules

--- src/theme/config/semantic-configs.ts
+++ src/theme/config/semantic-configs.ts
@@ -7,25 +7,25 @@ import type { SemanticTokenConfig } from './color-config-dsl.js'
  */
 export const variablesConfig: SemanticTokenConfig = {
   rules: {
-    variable: (p: UniversalPalette) => token(p.chromatic.blue.main), // c0caf5 -> blue.main
+    variable: (p: UniversalPalette) => token(p.chromatic.blue.main),
     'variable.readonly': (p: UniversalPalette) =>
-      italicToken(p.chromatic.blue.main), // c0caf5 -> blue.main
+      italicToken(p.chromatic.blue.main),
     'variable.declaration': (p: UniversalPalette) =>
-      token(p.chromatic.purple.main), // bb9af7 -> purple.main
-    parameter: (p: UniversalPalette) => token(p.chromatic.amber.main), // d9d4cd -> close to amber.main
+      token(p.chromatic.purple.main),
+    parameter: (p: UniversalPalette) => token(p.chromatic.amber.main),
     'parameter.readonly': (p: UniversalPalette) =>
-      italicToken(p.chromatic.amber.main), // d9d4cd -> close to amber.main
+      italicToken(p.chromatic.amber.main),
     'parameter.declaration': (p: UniversalPalette) =>
-      token(p.chromatic.orange.main), // e0af68 -> orange.main
-    property: (p: UniversalPalette) => token(p.chromatic.cyan.main), // 7dcfff -> cyan.main
+      token(p.chromatic.orange.main),
+    property: (p: UniversalPalette) => token(p.chromatic.cyan.main),
     'property.readonly': (p: UniversalPalette) =>
-      italicToken(p.chromatic.cyan.main), // 7dcfff -> cyan.main
+      italicToken(p.chromatic.cyan.main),
     'property.declaration': (p: UniversalPalette) =>
-      token(p.chromatic.teal.main), // 73daca -> teal.main
+      token(p.chromatic.teal.main),
     'variable.defaultLibrary': (p: UniversalPalette) =>
-      token(p.chromatic.cyan.light), // 2ac3de -> close to cyan.light
+      token(p.chromatic.cyan.light),
     'variable.declaration.readonly': (p: UniversalPalette) =>
-      italicToken(p.chromatic.purple.main), // bb9af7 -> purple.main
+      italicToken(p.chromatic.purple.main),
   },
 }
 
@@ -34,15 +34,15 @@ export const variablesConfig: SemanticTokenConfig = {
  */
 export const functionsConfig: SemanticTokenConfig = {
   rules: {
-    function: (p: UniversalPalette) => token(p.chromatic.blue.main), // 7aa2f7 -> blue.main
+    function: (p: UniversalPalette) => token(p.chromatic.blue.main),
     'function.declaration': (p: UniversalPalette) =>
-      boldToken(p.chromatic.blue.main), // 7aa2f7 -> blue.main
+      boldToken(p.chromatic.blue.main),
     'function.defaultLibrary': (p: UniversalPalette) =>
-      token(p.chromatic.cyan.main), // 2ac3de -> close to cyan.main
-    method: (p: UniversalPalette) => token(p.chromatic.blue.main), // 7aa2f7 -> blue.main
+      token(p.chromatic.cyan.main),
+    method: (p: UniversalPalette) => token(p.chromatic.blue.main),
     'method.declaration': (p: UniversalPalette) =>
-      boldToken(p.chromatic.blue.main), // 7aa2f7 -> blue.main
-    constructor: (p: UniversalPalette) => boldToken(p.chromatic.yellow.main), // e0af68 -> orange.main (closest)
+      boldToken(p.chromatic.blue.main),
+    constructor: (p: UniversalPalette) => boldToken(p.chromatic.yellow.main),
   },
 }
 
@@ -51,9 +51,9 @@ export const functionsConfig: SemanticTokenConfig = {
  */
 export const typesConfig: SemanticTokenConfig = {
   rules: {
-    type: (p: UniversalPalette) => boldToken(p.chromatic.teal.main), // teal.light is closest to 0db9d7
-    interface: (p: UniversalPalette) => boldToken(p.chromatic.teal.main), // teal.light is closest to 0db9d7
-    class: (p: UniversalPalette) => boldToken(p.chromatic.blue.main), // c0caf5 -> blue.main (closest)
+    type: (p: UniversalPalette) => boldToken(p.chromatic.teal.main),
+    interface: (p: UniversalPalette) => boldToken(p.chromatic.teal.main),
+    class: (p: UniversalPalette) => boldToken(p.chromatic.blue.main),
   },
 }
 
@@ -62,8 +62,8 @@ export const typesConfig: SemanticTokenConfig = {
  */
 export const modifiersConfig: SemanticTokenConfig = {
   rules: {
-    static: (p: UniversalPalette) => italicToken(p.chromatic.blue.main), // 7aa2f7 -> blue.main
-    readonly: (p: UniversalPalette) => italicToken(p.chromatic.cyan.main), // 7dcfff -> cyan.main
+    static: (p: UniversalPalette) => italicToken(p.chromatic.blue.main),
+    readonly: (p: UniversalPalette) => italicToken(p.chromatic.cyan.main),
   },
 }
 
@@ -72,11 +72,11 @@ export const modifiersConfig: SemanticTokenConfig = {
  */
 export const literalsConfig: SemanticTokenConfig = {
   rules: {
-    string: (p: UniversalPalette) => token(p.chromatic.green.main), // 9ece6a -> green.main
-    number: (p: UniversalPalette) => token(p.chromatic.orange.main), // ff9e64 -> orange.main
-    boolean: (p: UniversalPalette) => token(p.chromatic.red.main), // f7768e -> red.main
-    'string.escape': (p: UniversalPalette) => token(p.chromatic.yellow.main), // bb9af7 -> purple.main (closest)
-    'string.template': (p: UniversalPalette) => token(p.chromatic.green.main), // 9ece6a -> green.main
+    string: (p: UniversalPalette) => token(p.chromatic.green.main),
+    number: (p: UniversalPalette) => token(p.chromatic.orange.main),
+    boolean: (p: UniversalPalette) => token(p.chromatic.red.main),
+    'string.escape': (p: UniversalPalette) => token(p.chromatic.yellow.main),
+    'string.template': (p: UniversalPalette) => token(p.chromatic.green.main),
   },
 }
 
@@ -87,21 +87,21 @@ export const frameworksConfig: SemanticTokenConfig = {
   rules: {
     // React/Vue/Angular компоненты
     'class.component': (p: UniversalPalette) =>
-      boldToken(p.chromatic.teal.main), // 0db9d7 -> teal.main
+      boldToken(p.chromatic.teal.main),
     'interface.component': (p: UniversalPalette) =>
-      boldToken(p.chromatic.teal.main), // 0db9d7 -> teal.main
+      boldToken(p.chromatic.teal.main),
 
     // Декораторы
-    decorator: (p: UniversalPalette) => italicToken(p.chromatic.blue.main), // 7aa2f7 -> blue.main
+    decorator: (p: UniversalPalette) => italicToken(p.chromatic.blue.main),
 
     // Макросы
-    macro: (p: UniversalPalette) => boldToken(p.chromatic.purple.main), // bb9af7 -> purple.main
+    macro: (p: UniversalPalette) => boldToken(p.chromatic.purple.main),
 
     // Namespace
-    namespace: (p: UniversalPalette) => token(p.chromatic.blue.main), // 0db9d7 -> blue.main (closest)
+    namespace: (p: UniversalPalette) => token(p.chromatic.blue.main),
 
     // Enum
-    enum: (p: UniversalPalette) => boldToken(p.chromatic.orange.main), // ff9e64 -> orange.main
-    enumMember: (p: UniversalPalette) => token(p.chromatic.orange.light), // ff9e64 -> orange.light (closest)
+    enum: (p: UniversalPalette) => boldToken(p.chromatic.orange.main),
+    enumMember: (p: UniversalPalette) => token(p.chromatic.orange.light),
   },
 }
